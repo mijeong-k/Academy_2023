b79d87190fd175c23eff1f8f0299d433b853a606
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-02 14:31:45 +0900

    강의추가(Integer.parseInt)

---
+++ Acadamy/src/p230502/P2_plus2.java
@@ -0,0 +1,15 @@
+package p230502;
+
+public class P2_plus2 {
+
+	public static void main(String[] args) {
+		String S1 = new String("123");
+		String S2 = new String("56");
+		
+		int num1 = Integer.parseInt(S1);
+		int num2 = Integer.parseInt(S2);
+		
+		System.out.println(num1+num2);
+	}
+
+}
